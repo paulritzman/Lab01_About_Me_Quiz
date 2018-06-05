90f363ba01b4a18271b1b077f6ef659712efe0d7
paulritzman <ritzman.paul@gmail.com>
2018-06-05 10:24:17 -0700

    Completed second and third questions (city and dancer q's)

--- AboutMeQuiz/AboutMeQuiz/Program.cs
+++ AboutMeQuiz/AboutMeQuiz/Program.cs
@@ -11,9 +11,16 @@ namespace AboutMeQuiz
         static void Main(string[] args)
         {
             // Asks the user to guess my age - AskAgeQ returns -1 if no valid guess input.
-            int userGuess = AskAgeQ();
-            CheckAgeAnswer(userGuess);
+            int ageGuess = AskAgeQ();
+            CheckAgeAnswer(ageGuess);
 
+            // Asks the user to guess where I was born.
+            string cityGuess = AskBirthCityQ();
+            CheckBirthCityAnswer(cityGuess);
+
+            // Asks the user to guess if I was ever a dancer.
+            bool dancerGuess = AskDancerQ();
+            CheckDancerAnswer(dancerGuess);
 
             Console.ReadLine();
         }
@@ -31,7 +38,7 @@ namespace AboutMeQuiz
             }
             catch (Exception e)
             {
-                Console.WriteLine($"\nYou did not input a valid number: {e}\n");
+                Console.WriteLine($"\nYou did not input a valid, positive number: {e}\n");
             }
 
             // return -1 as CheckAgeAnswer is called next, and age can never be a negative - so will always be false.
@@ -40,15 +47,71 @@ namespace AboutMeQuiz
 
         static void CheckAgeAnswer(int guessedAge)
         {
-            int correctAge = 25;
+            int currentAge = 25;
 
-            if (guessedAge == correctAge)
+            if (guessedAge == currentAge)
             {
                 Console.WriteLine("Correct!\n");
             }
             else
             {
-                Console.WriteLine($"Sorry, that was incorrect. I am actually {correctAge}.\n");
+                Console.WriteLine($"Sorry, that was incorrect. I am actually {currentAge}.\n");
+            }
+        }
+
+        static string AskBirthCityQ()
+        {
+            Console.Write("Which city was I born in? ");
+            
+            // Converts user input to lower case, in order to simplify the comparison with correctCity in CheckBirthCityAnswer().
+            string guess = Console.ReadLine().ToLower();
+            return guess;
+        }
+
+        static void CheckBirthCityAnswer(string guessedCity)
+        {
+            string birthCity = "everett";
+
+            if (guessedCity == birthCity)
+            {
+                Console.WriteLine("Correct!\n");
+            }
+            else
+            {
+                Console.WriteLine($"Incorrect, I was actually born in {birthCity}.\n");
+            }
+        }
+
+        static bool AskDancerQ()
+        {
+            Console.Write("Was I ever a dancer (type 1 for yes, or 2 for no)? ");
+
+            string guess = Console.ReadLine();
+
+            if (guess == "1")
+            {
+                return true;
+            }
+            else if (guess != "2")
+            {
+                Console.WriteLine("You did not enter \"1\" or \"2\".\n");
+            }
+
+            // Returns false if anything other than "1" is input.
+            return false;
+        }
+
+        static void CheckDancerAnswer(bool guessedDancer)
+        {
+            bool wasDancer = true;
+
+            if (guessedDancer == wasDancer)
+            {
+                Console.WriteLine("Correct!\n");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect, I was a Ukrainian dancer for 3 years.\n");
             }
         }
     }
